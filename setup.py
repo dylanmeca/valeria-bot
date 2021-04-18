ca2378f4a4a1ece474cefe90a516a9642a47261c
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-04-18 17:14:33 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     version="1.0",
     author="dylan14567",
     author_email="",
-    description="valeria es un chatbot y es tu asistente virtual",
+    description="valeria es un chatbot y es tu asistente virtual que te puede ayudar",
     long_description=README,
     long_description_content_type="text/markdown",
     url="https://github.com/dylan14567/valeria",
