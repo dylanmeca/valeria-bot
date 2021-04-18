0bf18f07b7d0d8406e71388d2994728d9488d320
Dylan Meca <55631040+dylan14567@users.noreply.github.com>
2021-04-18 18:38:22 -0500

    Update pairs.py

--- valeriabot/pairs.py
+++ valeriabot/pairs.py
@@ -63,7 +63,7 @@ class pairs:
                  ["valeria: linux es un kernel, para crear sistemas operativos", "valeria: linux es un nucleo y con eso puedes crear una distribucion linux",] 
                 ],
                 [
-                 r"que opinas del area 51 (.*)| (.*) opinas del area 51",
+                 r"que opinas del area 51 (.*)| que opinas del area 51",
                  ["valeria: el area 51 es un lugar misterioso, se dice que hay aliens", "valeria: opino que es un lugar misterioso y que no debemos ir, ya que la curiosidad mato al gato",]
                 ],
                 [
